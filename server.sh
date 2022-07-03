aad7ca8ff2f9e61579c8db9dfaab92d90bf63a79
Gabriel Brito <gabrielbritodacruz@gmail.com>
2022-07-03 20:51:17 +0000

    Adiciona elementos visuais e informações de usabilidade

--- server.sh
+++ server.sh
@@ -3,7 +3,14 @@
 #Verifica se o script está sendo rodado no modo root, caso contrário
 #informa ao usuário que ele precisa de privilégios.
 if [[ $USER == "root" ]]; then
-    echo "[+] Configurando o Firewall..."
+
+    echo -e " --------------------------------------- "
+    echo
+    sleep 1
+    echo -e " \033[31;7;5;107m [+] \033[0m Configurando o Firewall..."
+    echo
+    sleep 1
+    echo -e " --------------------------------------- "
 
     #Interface bridge.
     placa1=`ip a | awk -F : '{print $2}' | grep "en" | head -n 1`
@@ -11,15 +18,18 @@ if [[ $USER == "root" ]]; then
     placa2=`ip a | awk -F : '{print $2}' | grep "en" | tail -n 1`
 
     #Faz com que as duas placas sejam ativadas.
+    echo "[+] Ativando placa 1."
     ip link set $placa1 up
+    echo "[+] Ativando placa 2."
     ip link set $placa2 up
 
     #Faz com que a placa host-only faça a busca DHCP por IP.
+    echo "Realizando requisição por IP na placa 2."
     dhclient $placa2
 
     #Ativa o encanmihamento de pacotes.
     echo 1 > /proc/sys/net/ipv4/ip_forward
-    
+
     #Adiciona uma regra no IPTABLES para a interface bridge.
     iptables -t nat -A POSTROUTING -o $placa1 -j MASQUERADE
 
