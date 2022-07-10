4d46a7f3a5c239ba93a8d8b55e541d4179f27483
Gabriel Brito <34654875+gabriel-bri@users.noreply.github.com>
2022-07-10 12:26:38 -0300

    Adiciona novas funcionalidades e configurações.

--- firewall.sh
+++ firewall.sh
@@ -7,7 +7,7 @@ if [[ $USER == "root" ]]; then
     echo -e " --------------------------------------- "
     echo
     sleep 1
-    echo -e "\033[31;7;5;107m [+] \033[0m Configurando o Firewall..."
+    echo -e "\033[31;7;5;107m[+]\033[0m Configurando o Firewall..."
     echo
     sleep 1
     echo -e " --------------------------------------- "
@@ -39,28 +39,118 @@ if [[ $USER == "root" ]]; then
     iptables -P INPUT DROP
     iptables -P FORWARD DROP
     iptables -P OUTPUT DROP
-    
-    #Liberando conexõe SSH.
+
+    #Liberando conexões SSH.
     echo "[+] Liberando conexão SSH na porta 22."
     iptables -A INPUT -p tcp -m tcp --dport 22 -j ACCEPT
     iptables -A OUTPUT -p tcp -m tcp --sport 22 -j ACCEPT
+
+    #Liberando protocolo ICMP.
+    echo "[+] Liberando ping."
+    iptables -A INPUT -p icmp -j ACCEPT
+    iptables -A OUTPUT -p icmp -j ACCEPT
+
+    #Liberando conexõe SSH.
+    echo "[+] Liberando conexões DNS na porta 53."
+    iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
+    iptables -A INPUT -p udp --sport 53 -j ACCEPT
 
     #Ativa o encanmihamento de pacotes.
     echo "[+] Ativando encaminhamento de pacotes."
     echo 1 > /proc/sys/net/ipv4/ip_forward
 
-    #Adiciona uma regra no IPTABLES para a interface bridge.
+    Adiciona uma regra no IPTABLES para a interface bridge.
     echo "[+] Adicionando mascaramento IP."
     iptables -t nat -A POSTROUTING -o $placa1 -j MASQUERADE
+
+    ipPlaca1=`ip a | grep "en" | grep "inet" |  awk -F  " " '{print $2}' | head -n 1`
+    ipPlaca2=`ip a | grep "en" | grep "inet" |  awk -F  " " '{print $2}' | head -n 2 | tail -n 1`
+    ipPlaca3=`ip a | grep "en" | grep "inet" |  awk -F  " " '{print $2}' | tail -n 1`
+
+    redePlaca2=`ipcalc $ipPlaca2 | grep "Network" |  awk -F  " " '{print $2}'`
+    redePlaca3=`ipcalc $ipPlaca3 | grep "Network" |  awk -F  " " '{print $2}'`
+    ipRede2=`ipcalc $ipPlaca2 | grep "Network" |  awk -F  " " '{print $2}' | rev | cut -c4- | rev`
+    restoString=`ipcalc $ipPlaca2 | grep "Network" |  awk -F  " " '{print $2}' | awk -F "/" '{print $2}'
+
+    echo "[+] Trabalhando em configurações do Squid..."
+    #Susbtitue a rede que está no arquivo pela rede na qual o script está sendo executado.
+    sed -i -e 's/'192.168.150.0\\/24'\b/'$ipRede2\\/$restoString'/g' squid.conf
+
+    #Cria um backup do arquivo original do Squid.
+    echo "[+] Criando backup das configurações do Squid."
+    cp /etc/squid/squid.conf /etc/squid/squid.conf.bkp
+    cp squid.conf /etc/squid/squid.conf
 
     echo "[+] Recarregando arquivos de configuração do Squid."
     sudo invoke-rc.d squid reload
 
     echo "[+] Reiniciando o Squid..."
     sudo invoke-rc.d squid restart
-#ip a | grep en | grep inet |  awk -F  " '{print $2}
-#ipcalc $ip | grep Network |  awk -F  " '{print $2}
 
+    echo "Placa $placa1 tem o IP de $ipPlaca1"
+    echo "Placa $placa2 tem o IP de $ipPlaca2"
+    echo "Placa $placa3 tem o IP de $ipPlaca3"
+
+    #Aplicando configurações de Iptables para o Cliente.
+    echo "[+] Aplicando configurações para o Cliente."
+
+    #Permissão do SSH.
+    iptables -A FORWARD -s $redePlaca2 -p tcp -m tcp --sport 22 -j ACCEPT
+    iptables -A FORWARD -s $redePlaca2 -p tcp -m tcp --dport 22 -j ACCEPT
+
+    #Permissão de HTTP.
+    iptables -A FORWARD -s $redePlaca2 -p tcp -m tcp --sport 80 -j ACCEPT
+    iptables -A FORWARD -s $redePlaca2 -p tcp -m tcp --dport 80 -j ACCEPT
+    iptables -A FORWARD -d $redePlaca2 -p tcp -m tcp --sport 80 -j ACCEPT
+    iptables -A FORWARD -d $redePlaca2 -p tcp -m tcp --dport 80 -j ACCEPT
+
+    #Permissão de DNS.
+    iptables -A FORWARD -d $redePlaca2 -p udp -m udp --dport 53 -j ACCEPT
+    iptables -A FORWARD -d $redePlaca2 -p udp -m udp --sport 53 -j ACCEPT
+    iptables -A FORWARD -s $redePlaca2 -p udp -m udp --dport 53 -j ACCEPT
+    iptables -A FORWARD -s $redePlaca2 -p udp -m udp --sport 53 -j ACCEPT
+
+    #Permissão de HTTPS.
+    iptables -A FORWARD -d $redePlaca2 -p tcp --dport 443 -j ACCEPT
+    iptables -A FORWARD -d $redePlaca2 -p tcp --sport 443 -j ACCEPT
+    iptables -A FORWARD -s $redePlaca2 -p tcp --sport 443 -j ACCEPT
+    iptables -A FORWARD -s $redePlaca2 -p tcp --dport 443 -j ACCEPT
+
+    #Permissão para o ICMP.
+    iptables -A FORWARD -d $redePlaca2 -p icmp -j ACCEPT
+    iptables -A FORWARD -s $redePlaca2 -p icmp -j ACCEPT
+
+    #Aplicando configurações para o Servidor.
+    echo "Verifique o IP do servidor web e digite ele abaixo:"
+    read ipServidor
+
+    echo "[+] Aplicando configurações para o Servidor."
+    #Desviando tráfego par a porta 80 do servidor.
+    iptables -t nat -A PREROUTING -d $ipPlaca1 -p tcp --dport 80 -j DNAT --to-destination $ipServidor:80
+
+    #Permissão de HTTP.
+    iptables -A FORWARD -s $redePlaca3 -p tcp -m tcp --sport 80 -j ACCEPT
+    iptables -A FORWARD -s $redePlaca3 -p tcp -m tcp --dport 80 -j ACCEPT
+    iptables -A FORWARD -d $redePlaca3 -p tcp -m tcp --sport 80 -j ACCEPT
+    iptables -A FORWARD -d $redePlaca3 -p tcp -m tcp --dport 80 -j ACCEPT
+
+    #Permissão de DNS.
+    iptables -A FORWARD -d $redePlaca3 -p udp --dport 53 -j ACCEPT
+    iptables -A FORWARD -d $redePlaca3 -p udp --sport 53 -j ACCEPT
+    iptables -A FORWARD -s $redePlaca3 -p udp --sport 53 -j ACCEPT
+    iptables -A FORWARD -s $redePlaca3 -p udp --dport 53 -j ACCEPT
+
+    #Permissão de HTTPS.
+    iptables -A FORWARD -d $redePlaca3 -p tcp --dport 443 -j ACCEPT
+    iptables -A FORWARD -d $redePlaca3 -p tcp --sport 443 -j ACCEPT
+    iptables -A FORWARD -s $redePlaca3 -p tcp --sport 443 -j ACCEPT
+    iptables -A FORWARD -s $redePlaca3 -p tcp --dport 443 -j ACCEPT
+
+    #Permissão para o ICMP.
+    iptables -A FORWARD -d $redePlaca3 -p icmp -j ACCEPT
+    iptables -A FORWARD -s $redePlaca3 -p icmp -j ACCEPT
+
+    echo "[+] Servidor de Firewall configurando com sucesso."
 else
     #Mensagem de erro.
     echo "Você precisa executar este script como root."
